fcb30637b9028183d3249e3ddaab53341e1dac5d
TheUltimateOptimist <digitalConfidence@web.de>
2023-04-15 06:31:09 +0200

    finished adding shared state

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use axum::{Extension, debug_handler};
-use parser::{self, ParseError, Command, CommandNode, DataType, Optional, Output, Parameter, PomodoroTimer, TreeNode, parse_with_tree};
+use parser::{self, Output, TreeNode};
 use parser_macros::{command, register, command_n};
 use axum::extract::{Path, State};
 use axum::{extract::ws::{WebSocket, WebSocketUpgrade, Message}, Router, routing::{get, post}, response::{Response, IntoResponse}, http::StatusCode};
@@ -95,10 +95,11 @@ async fn authorise(token: &str, project_id: &str) -> Result<Claims, Response> {
 async fn ws_handler(socket_up: WebSocketUpgrade, Path(token): Path<String>, State(pool): State<Pool<Postgres>>) -> Result<Response, Response> {
     let claims = authorise(&token, "personal-743af").await?;
     println!("{:?}", claims);
-    Ok(socket_up.on_upgrade(ws))
+    //ws.on_upgrade(move |socket| handle_socket(socket, addr));
+    Ok(socket_up.on_upgrade(|socket| ws(socket, pool)))
 }
 
-async fn ws(mut socket: WebSocket) {
+async fn ws(mut socket: WebSocket, pool: Pool<Postgres>) {
     while let Some(msg) = socket.recv().await {
         let msg = if let Ok(msg) = msg {
             msg
@@ -107,7 +108,7 @@ async fn ws(mut socket: WebSocket) {
             return;
         };
         let message_text = msg.into_text().unwrap();
-        let outputs = match parse(&message_text).await {
+        let outputs = match parse(&message_text, parser::State(pool.clone())).await {
             Ok(value) => value,
             Err(err) => vec![Output::Error(err.to_string())],
 
@@ -124,33 +125,31 @@ async fn ws(mut socket: WebSocket) {
 
 
 register! {
-    "logout": logout
-    "lofi": lofi
-    "some"
-        "another"
-            "lol": lol
-    "yeah": yeah
-    "table": table
-    "tree": tree
-    "pomodoro": pomodoro
+----"logout": logout
+----"lofi": lofi
+----"some"
+--------"another"
+------------"lol": lol
+----"yeah": yeah
+----"table": table
+----"tree": tree
+----"pomodoro": pomodoro
 }
 
-
-
 #[command]
-fn logout() -> Output {
+fn logout(_: parser::State<Pool<Postgres>>) -> Output {
     return Output::Logout;
 }
 
 #[command(duration = 1620, pause = 180)]
-fn pomodoro(topic_id: i32, duration: i32, pause: i32) -> Output {
+fn pomodoro(topic_id: i32, duration: i32, pause: i32, _: parser::State<Pool<Postgres>>) -> Output {
     return Output::PomodoroTimer(
-        PomodoroTimer { duration: duration, pause: pause, topic_id: topic_id, topic_name: "some name".to_owned() }
+        parser::PomodoroTimer { duration: duration, pause: pause, topic_id: topic_id, topic_name: "some name".to_owned() }
     )
 }
 
 #[command]
-fn tree() -> Output {
+fn tree(_: parser::State<Pool<Postgres>>) -> Output {
     return Output::Tree(
         TreeNode { name: "first".to_owned(), children: Some(vec![
             TreeNode {
@@ -168,7 +167,7 @@ fn tree() -> Output {
 }
 
 #[command]
-fn table() -> Output {
+fn table(parser::State(pool): parser::State<Pool<Postgres>>) -> Output {
     return Output::Table(parser::Table{
         title: "Some Title".to_owned(),
         data: vec![vec!["first".to_owned(), "second".to_owned(), "third".to_owned()], vec!["one".to_owned(), "two".to_owned(), "three".to_owned()], vec!["four".to_owned(), "five".to_owned(), "six".to_owned()]],
@@ -177,18 +176,18 @@ fn table() -> Output {
 
 
 #[command(two = 2, three = "sdfkl")]
-fn lofi(one: String, two: i32, three: String) -> Output {
+fn lofi(one: String, two: i32, three: String, _: parser::State<Pool<Postgres>>) -> Output {
     println!("lofi");
     return Output::Empty;
 }
 
 #[command_n]
-fn yeah() -> Vec<Output> {
+fn yeah(_: parser::State<Pool<Postgres>>) -> Vec<Output> {
     return vec![Output::Text("one".to_owned()), Output::Text("two".to_owned())];
 }
 
 #[command]
-fn lol() -> Output {
+fn lol(_: parser::State<Pool<Postgres>>) -> Output {
     println!("lol");
     return Output::Empty;
 }
