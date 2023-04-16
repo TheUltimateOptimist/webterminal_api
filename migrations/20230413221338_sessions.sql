da268fcee912e670f968fb08f19e7436a438a8b9
TheUltimateOptimist <digitalConfidence@web.de>
2023-04-16 22:51:45 +0200

    changed sessions table schema

--- migrations/20230413221338_sessions.sql
+++ migrations/20230413221338_sessions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
     start double precision NOT NULL,
-    "end" double precision NOT NULL,
+    duration INTEGER NOT NULL,
     topic_id INTEGER REFERENCES topics(id)
 )
